5ee8c3bb7f8747a6fd67785e536af844bbc7dbbe
Imron Alston <imron@imralsoftware.com>
2015-03-31 05:29:14 -0400

    updated to support default values, error responses, and ssl verification

--- lib/fluent/plugin/out_scalyr.rb
+++ lib/fluent/plugin/out_scalyr.rb
@@ -1,8 +1,9 @@
 
-require 'securerandom'
+require 'fluent/plugin/scalyr-exceptions'
 require 'json'
 require 'net/http'
 require 'net/https'
+require 'securerandom'
 
 module Scalyr
   class FluentLogger < Fluent::BufferedOutput
@@ -15,17 +16,36 @@ module Scalyr
     config_param :ssl_verify_peer, :bool, :default => true
     config_param :ssl_verify_depth, :integer, :default => 5
 
+    config_set_default :retry_limit, 5 #try a maximum of 5 times before discarding
+    config_set_default :retry_wait, 5 #wait a minimum of 5 seconds before retrying again
+    config_set_default :max_retry_wait,  30 #wait a maximum of 30 seconds per retry
+    config_set_default :flush_interval, 5 #default flush interval of 5 seconds
+
     def configure( conf )
+      #need to call this before super because there doesn't seem to be any other way to
+      #set the default value for the buffer_chunk_limit, which is created and configured in super
+      if !conf.key? "buffer_chunk_limit"
+        conf["buffer_chunk_limit"] = "256k";
+      end
       super
+
+      if @buffer.buffer_chunk_limit > 1024*1024
+        $log.warn "Buffer chunk size is greater than 1Mb.  This may result in requests being rejected by Scalyr"
+      end
+
       @last_timestamp = 0
       @add_events_uri = URI @add_events
 
-      raise Fluent::ConfigError, "num_threads is currently limited to 1. You specified #{@num_threads}." if @num_threads > 1
+      #forcibly limit the number of threads to 1 for now, because parts of the write method aren't thread safe
+      #raise Fluent::ConfigError, "num_threads is currently limited to 1. You specified #{@num_threads}." if @num_threads > 1
     end
 
     def start
       super
+      #Generate a session id.  This will be called once for each <match> in fluent.conf that uses scalyr
       @session = SecureRandom.uuid
+
+      #hash of scalyr thread ids, keyed by fluentd tag name
       @thread_ids = Hash.new
       @next_id = 1
     end
@@ -34,21 +54,97 @@ module Scalyr
       [tag, time, record].to_msgpack
     end
 
+    #called by fluentd when a chunk of log messages is ready
     def write( chunk )
+      body = self.build_add_events_body( chunk )
+      response = self.post_request( @add_events_uri, body )
+      self.handle_response( response )
+    end
 
+
+
+    #explicit function to convert to nanoseconds
+    #will make things easier to maintain if/when fluentd supports higher than second resolutions
+    def to_nanos( seconds )
+      seconds * 10**9
+    end
+
+    #explicit function to convert to milliseconds
+    #will make things easier to maintain if/when fluentd supports higher than second resolutions
+    def to_millis( seconds )
+      seconds * 10**6
+    end
+
+    def post_request( uri, body )
+
+      https = Net::HTTP.new( uri.host, uri.port )
+      https.use_ssl = true
+
+      #verify peers to prevent potential MITM attacks
+      if @ssl_verify_peer
+        https.ca_file = @ssl_ca_bundle_path
+        https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        https.verify_depth = @ssl_verify_depth
+      end
+
+      post = Net::HTTP::Post.new uri.path
+      post.add_field( 'Content-Type', 'application/json' )
+
+      post.body = body
+
+      https.request( post )
+
+    end
+
+    def handle_response( response )
+      $log.debug "Response Code: #{response.code}"
+      $log.debug "Response Body: #{response.body}"
+
+      response_hash = JSON.parse( response.body )
+
+      #make sure the JSON reponse has a "status" field
+      if !response_hash.key? "status"
+        $log.debug "JSON response does not contain status message"
+        raise Scalyr::ServerError
+      end
+
+      status = response_hash["status"]
+
+      if status.start_with? "error"
+        if status =~ %r"/client/"i
+          raise Scalyr::ClientError
+        else #don't check specifically for server, we assume all non-client errors are server errors
+          raise Scalyr::ServerError
+        end
+      elsif !response.code.include? "200" #response code is a string not an int
+        raise Scalyr::ServerError
+      end
+    end
+
+    def build_add_events_body( chunk )
+
+      #set of unique scalyr threads for this chunk
+      current_threads = Hash.new
+
+      #create a Scalyr event object for each record in the chunk
       events = Array.new
-
       chunk.msgpack_each {|(tag,time,record)|
 
+        #add a new thread id if we haven't seen this tag before
         if !@thread_ids.key? tag
           @thread_ids[tag] = @next_id
           @next_id += 1
         end
 
-        timestamp = time * 10**9
+        #then update the map of threads for this chunk
+        current_threads[tag] = @thread_ids[tag]
+
+        #ensure timestamp is at least 1 nanosecond greater than the last one
+        timestamp = self.to_nanos( time )
         timestamp = [timestamp, @last_timestamp + 1].max
         @last_timestamp = timestamp
 
+        #append to list of events
         events << { :thread => @thread_ids[tag].to_s,
                     :ts => timestamp.to_s,
                     :attrs => record
@@ -56,16 +152,15 @@ module Scalyr
 
       }
 
+      #build the scalyr thread objects
       threads = Array.new
-
-
-      @thread_ids.each do |tag, id|
+      current_threads.each do |tag, id|
         threads << { :id => id,
                      :name => "Fluentd: #{tag}"
                    }
       end
 
-      current_time = Fluent::Engine.now * 10**6
+      current_time = self.to_millis( Fluent::Engine.now )
 
       body = { :token => @api_write_token,
                   :client_timestamp => current_time.to_s,
@@ -74,34 +169,12 @@ module Scalyr
                   :threads => threads
                 }
 
-      @session_info = Hash.new
-      @session_info[:rubyThread] = Thread.current.object_id.to_s
-
+      #add session_info hash if it exists
       if @session_info
         body[:sessionInfo] = @session_info
       end
 
-      https = Net::HTTP.new( @add_events_uri.host, @add_events_uri.port )
-      https.use_ssl = true
-
-      if @ssl_verify_peer
-        https.ca_file = @ssl_ca_bundle_path
-        https.verify_mode = OpenSSL::SSL::VERIFY_PEER
-        https.verify_depth = @ssl_verify_depth
-      end
-
-      post = Net::HTTP::Post.new @add_events_uri.path
-      post.add_field( 'Content-Type', 'application/json' )
-      post.body = body.to_json
-
-      response = https.request( post )
-
-      $log.debug "Post size: #{post.body.length/1024/1024}m"
-
-      $log.debug "Response Code: #{response.code}"
-      $log.debug "Response Body: #{response.body}"
-
-
+      body.to_json
     end
 
   end
---
+++ lib/fluent/plugin/scalyr-exceptions.rb
@@ -0,0 +1,5 @@
+
+module Scalyr
+  class ClientError < StandardError; end
+  class ServerError < StandardError; end
+end
